2e749815afca224a9ebd3699fc0e7fab1a748aad
Mahmud Lukman <mistamann49@gmail.com>
2023-09-18 11:36:54 +0100

    GET, PATCH & DELETE endpoints added

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,6 +7,9 @@ import { usePathname, useRouter } from 'next/navigation'
 
 const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
   const { data: session } = useSession();
+  const pathname = usePathname()
+  const router = useRouter()
+
   const [copied, setCopied] = useState('')
   const handleCopy = () => {
     setCopied(post.prompt)
@@ -49,6 +52,22 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
       <p className='font-inter text-sm blue_gradient cursor-pointer'
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >{post.tag}</p>
+      {session?.user.id === post.creator._id && pathname === '/profile' && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p
+            className='font-inter text-sm green_gradient cursor-pointer'
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className='font-inter text-sm orange_gradient cursor-pointer'
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
